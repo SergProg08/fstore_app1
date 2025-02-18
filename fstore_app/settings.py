4868b87c69ce338c9a7a7a2712a76166916a4174
Serg <rybakvasilii1608@gmail.com>
2025-02-18 14:40:48 +0300

    class based views

--- fstore_app/settings.py
+++ fstore_app/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import MEDIA_ROOT, MEDIA_URL
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -157,3 +156,4 @@ INTERNAL_IPS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 LOGIN_URL = '/user/login'
+LOGIN_REDIRECT_URL = '/'
